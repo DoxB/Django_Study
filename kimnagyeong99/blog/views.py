038f87cf52c66c600714652af351556babdca596
kimnagyeong <knk990928@gmail.com>
2024-10-08 17:54:48 +0900

    url

--- kimnagyeong99/blog/views.py
+++ kimnagyeong99/blog/views.py
@@ -5,4 +5,7 @@ def index(request):
     return render(request, 'index.html')  # templates 폴더 안의 index.html을 렌더링
 
 def portfolio(request):
-    return render(request, 'portfolio-details.html')  # templates 폴더 안의 index.html을 렌더링
+    return render(request, 'portfolio-details.html')  # templates 폴더 안의 portfolio-details.html을 렌더링
+
+def service_details(request):
+    return render(request, 'service-details.html')  # templates 폴더 안의 portfolio-details.html을 렌더링
